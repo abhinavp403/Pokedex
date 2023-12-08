c75f25088aa116bd34d9f6635739348667841db5
abhinavp403 <abhinavp403@gmail.com>
2023-12-07 23:14:04 -0500

    Loading and displaying pokemon list entries using pagination

--- app/src/main/java/dev/abhinav/pokedex/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/dev/abhinav/pokedex/pokemonlist/PokemonListViewModel.kt
@@ -3,17 +3,62 @@ package dev.abhinav.pokedex.pokemonlist
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.abhinav.pokedex.data.models.PokedexListEntry
 import dev.abhinav.pokedex.repository.PokemonRepository
+import dev.abhinav.pokedex.util.Constants.PAGE_SIZE
+import dev.abhinav.pokedex.util.Resource
+import kotlinx.coroutines.launch
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
     private val repository: PokemonRepository
 ) : ViewModel() {
+
+    private var curPage = 0
+
+    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    fun loadPokemonPaginated() {
+        viewModelScope.launch {
+            isLoading.value = true
+            val result = repository.getPokemonList(PAGE_SIZE, curPage * PAGE_SIZE)
+            when(result) {
+                is Resource.Success -> {
+                    endReached.value = curPage * PAGE_SIZE >= result.data!!.count
+                    val pokedexEntries = result.data.results.mapIndexed { index, entry ->
+                        val number = if(entry.url.endsWith("/")) {
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+                        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+                        PokedexListEntry(entry.name.capitalize(Locale.ROOT), url, number.toInt())
+                    }
+                    curPage++
+
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokedexEntries
+                }
+                is Resource.Error -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+                }
+                else -> {}
+            }
+        }
+    }
 
     // Calculate dominant color of image to adjust background image with the dominant color
     fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
--- app/src/main/java/dev/abhinav/pokedex/util/Constants.kt
+++ app/src/main/java/dev/abhinav/pokedex/util/Constants.kt
@@ -3,4 +3,6 @@ package dev.abhinav.pokedex.util
 object Constants {
 
     const val BASE_URl = "https://pokeapi.co/api/v2/"
+
+    const val PAGE_SIZE = 20
 }
