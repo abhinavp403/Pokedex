10d97e3fea412f83448bf456cf8d78f86c00df33
abhinavp403 <abhinavp403@gmail.com>
2023-12-10 12:29:32 -0500

    Click on list item to open detail screen. Top portion of detail screen made

--- app/src/main/java/dev/abhinav/pokedex/MainActivity.kt
+++ app/src/main/java/dev/abhinav/pokedex/MainActivity.kt
@@ -15,8 +15,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import dev.abhinav.pokedex.pokemondetail.PokemonDetailScreen
 import dev.abhinav.pokedex.pokemonlist.PokemonListScreen
 import dev.abhinav.pokedex.ui.theme.PokedexTheme
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -52,7 +54,11 @@ class MainActivity : ComponentActivity() {
                         val pokemonName = remember {
                             it.arguments?.getString("pokemonName")
                         }
-
+                        PokemonDetailScreen(
+                            dominantColor = dominantColor,
+                            pokemonName = pokemonName?.toLowerCase(Locale.ROOT) ?: "",
+                            navController = navController
+                        )
                     }
                 }
             }
