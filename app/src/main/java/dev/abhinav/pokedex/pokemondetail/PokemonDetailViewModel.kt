3aafe1e389ccaa73beb85dea149677d842190669
abhinavp403 <abhinavp403@gmail.com>
2023-12-11 18:28:24 -0500

    Completed detail section with animation added

--- app/src/main/java/dev/abhinav/pokedex/pokemondetail/PokemonDetailViewModel.kt
+++ app/src/main/java/dev/abhinav/pokedex/pokemondetail/PokemonDetailViewModel.kt
@@ -1,33 +1,16 @@
 package dev.abhinav.pokedex.pokemondetail
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.palette.graphics.Palette
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.abhinav.pokedex.data.models.PokedexListEntry
 import dev.abhinav.pokedex.data.remote.response.Pokemon
 import dev.abhinav.pokedex.repository.PokemonRepository
-import dev.abhinav.pokedex.util.Constants.PAGE_SIZE
 import dev.abhinav.pokedex.util.Resource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonDetailViewModel @Inject constructor(
     private val repository: PokemonRepository
 ) : ViewModel() {
-
-    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
-    var loadError = mutableStateOf("")
-    var isLoading = mutableStateOf(false)
-    var endReached = mutableStateOf(false)
 
     suspend fun getPokemonInfo(name: String): Resource<Pokemon> {
         return repository.getPokemonInfo(name)
