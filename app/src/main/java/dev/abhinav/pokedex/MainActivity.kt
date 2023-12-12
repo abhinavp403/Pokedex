933f6daf43dcaf1b7a73594ed40fe9c63751578f
abhinavp403 <abhinavp403@gmail.com>
2023-12-11 19:49:28 -0500

    Cleaned up code

--- app/src/main/java/dev/abhinav/pokedex/MainActivity.kt
+++ app/src/main/java/dev/abhinav/pokedex/MainActivity.kt
@@ -3,12 +3,8 @@ package dev.abhinav.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -63,21 +59,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokedexTheme {
-        Greeting("Android")
     }
 }
